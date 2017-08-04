27108a3c890f6e100f839dcf3e5ab52700906073
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-04 19:00:12 +0100

    send email to the email microservice using seneca

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var async = require('async');
+var seneca = require('seneca');
 var router = express.Router();
 
 let Category = require('../models/category');
 let Blog = require('../models/blog');
+let config = require('../config');
 
 router.get('/', 
     function(req, res, next) {
@@ -13,10 +15,19 @@ router.get('/',
 });
 
 router.get('/page/:page(\\d+)', 
-    module.exports.page = function(req, res, next) {
-        
+    module.exports.page = function(req, res, next, params) {
+               
         data = {};
         data.title = 'Maxwell Obi';
+
+        if(params == 'subscribed')
+            data.scripts = `
+                <script>
+                    $(document).ready(function(){
+                        swal("Thank you for subscribing!", "You will now receive my newsletters on a monthly basis", "success");
+                    });
+                </script>
+                `;
 
         data.perPage = perPage = 10;
         var page = (req.params.page ? req.params.page : 1) - 1;
@@ -52,6 +63,31 @@ router.get('/contact',
     function(req, res, next) {
 
         res.render('contact', { title: 'Maxwell Obi\'s Contact'});
+});
+
+router.post('/', 
+    function(req, res, next) {
+        
+       req.checkBody('email', 'Email is required').isEmail();
+       req.sanitizeBody('email').escape();
+
+       req.getValidationResult()
+       .then((validation_result) => {
+            if(!validation_result.isEmpty()) return next(validation_result);
+
+            //send to the email microservice
+            seneca()
+            .client({ port: config.services.email, host: 'localhost', type: 'tcp', pin: 'role:email_service' })
+            .act('role:email_service, cmd:save_email', { email: req.body.email}, function(err, result){
+
+                if(err) return next(err);
+                    
+                else{
+                    return module.exports.page(req, res, next, 'subscribed');
+                }
+            });
+       });
+        
 });
 
 module.exports.route = router;
